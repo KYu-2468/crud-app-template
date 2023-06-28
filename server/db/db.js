cd9ba38dfe7b2e309f4f861ea4e03a454c85348d
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-06-28 03:22:23 -0700

    Update db connection logic

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,8 @@
 const Sequelize = require("sequelize");
 const { name } = require("../../package.json");
+require("dotenv").config();
+
+let databaseURL = null;
 
 const config = {
   logging: false,
@@ -9,18 +12,19 @@ if (process.env.LOGGING === "true") {
   delete config.logging;
 }
 
-//https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
-if (process.env.DATABASE_URL) {
+if (process.env.NODE_ENV === "production" && process.env.DATABASE_URL) {
+  databaseURL = process.env.DATABASE_URL;
+
+  //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
   config.dialectOptions = {
     ssl: {
       rejectUnauthorized: false,
     },
   };
+} else {
+  databaseURL = `postgres://localhost:5432/${name}`;
 }
 
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${name}`,
-  config
-);
+const db = new Sequelize(databaseURL, config);
 
 module.exports = db;
